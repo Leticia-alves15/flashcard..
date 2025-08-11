dfcfefbc48590b718d95c9b8e527195d3bf49406
hannie <carneiro.alves@escola.pr.gov.br>
2025-08-11 19:43:49 -0300

    gg

---
+++ assets/perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Biologia',
+    'Qual órgão humano consegue se regenerar?',
+    'O fígado, ele é o único órgão capaz de se regenerar completamente.'
+);
+
+criaCartao(
+    'Biologia',
+    'Quantos ossos tem o corpo humano?',
+    '206 ossos. Já os bebês nascem com cerca de 270, que se fundem com o crescimento.'
+);
+
+criaCartao(
+    'Biologia',
+    'Qual é o maior órgão humano?',
+    'A pele.'
+);
+
+criaCartao(
+    'Biologia',
+    'Qual o nome do líquido que lubrifica as articulações?',
+    'O nome é líquido sinovial.'
+);
+
+criaCartao(
+    'Biologia',
+    'Qual a função dos glóbulos vermelhos?',
+    'Transportar oxigênio pelo corpo.'
+);
